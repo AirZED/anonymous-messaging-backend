da0f173c520db200588e5b89674208e6d4533691
AirZED <regnalukpabio@gmail.com>
2023-07-30 06:52:57 +0100

    sec:more security fits

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.set('trust proxy', true);
+app.set('trust proxy', false);
 // protection against http attacks
 app.use(helmet());
 
